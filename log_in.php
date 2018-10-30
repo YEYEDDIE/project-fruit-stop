4e06f34f5233d1d272dbf93f9ac1c13ac77f0aa1
YEYEDDIE <edvard.seger@gmail.com>
2018-10-30 11:23:29 +0200

    code_1

--- log_in.php
+++ log_in.php
@@ -21,12 +21,12 @@ if (isset($_POST)&&!empty($_POST)){
 	
 	
 	$sql ="SELECT * FROM users 
-	WHERE user_email=:user_email AND
+	WHERE user_name=:user_name AND
 	user_password=:user_password
 	LIMIT 1";
 	
 	$row = [
-	':user_email' => $_POST['user_email'],
+	':user_name' => $_POST['user_name'],
 	':user_password' => md5($_POST['user_password'])
 	];
 	$res = $conn->prepare($sql);
@@ -37,7 +37,7 @@ if (isset($_POST)&&!empty($_POST)){
 	}else{
 		//$output = "User ok";
 		$_SESSION['user']=1;
-		header('Location: index.php');
+		header('Location: index_2.php');
 	}
 }
 
@@ -53,7 +53,7 @@ if (isset($_POST)&&!empty($_POST)){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
  
-    <title>Bootsrap login formulär!</title>
+    <title> login</title>
     <style>
     img { max-width: 100%; }
     h1 { text-align: center; }
@@ -68,8 +68,8 @@ if (isset($_POST)&&!empty($_POST)){
 				<?php if(!empty($output)){echo '<h3 class="alert alert-warning">'. $output. '</h3>';}?>
           <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="user_email" name="user_email" aria-describedby="emailHelp" placeholder="Enter email">
+              <label for="exampleInputusername1">Username</label>
+              <input type="name" class="form-control" id="user_name" name="user_name"  placeholder="Enter Username">
             </div>
             <div class="form-group">
               <label for="exampleInputPassword1">Password</label>
@@ -83,6 +83,7 @@ if (isset($_POST)&&!empty($_POST)){
 </div> <!-- / container -->
  <!-- Optional JavaScript -->
  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <p><a class="btn" href="index_1.html?=logout">Tillbaka till hemsidan</a></p>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
